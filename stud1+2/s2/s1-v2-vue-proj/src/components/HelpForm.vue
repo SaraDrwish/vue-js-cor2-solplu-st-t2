0558f29114a0bdfc248d238502121601e3a4443e
SaraDrwish <sooosoooadel555@gmail.com>
2024-03-18 13:39:23 +0200

    18-03-2024 till  v28 v2 vue-cli-service lint

--- stud1+2/s2/s1-v2-vue-proj/src/components/HelpForm.vue
+++ stud1+2/s2/s1-v2-vue-proj/src/components/HelpForm.vue
@@ -16,15 +16,35 @@ export default {
 };
 </script>
 
-<style scoped lang="scss">
-form,
+<style scoped>
 .HelpForm {
+  background: rgba(219, 181, 181, 0.747);
+  padding: 1rem;
+}
+form {
   display: flex;
   flex-direction: column;
   gap: 1rem;
   width: 60%;
-  background: rgb(65, 77, 48);
+  padding: 1rem;
+  background: rgb(143, 158, 122);
   color: rgb(54, 88, 88);
   font-size: 22px;
+  border-radius: 2rem;
+  margin: auto;
+}
+form input,
+form textarea {
+  outline: none;
+  border: none;
+  border-radius: 2rem;
+  padding: 1rem;
+}
+form input[type="button"] {
+  background: burlywood;
+  color: tomato;
+  width: 50%;
+  margin: auto;
+  cursor: pointer;
 }
 </style>
