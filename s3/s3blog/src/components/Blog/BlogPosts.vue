8a91db8b39a9a37767cd4eac938ad0019280bbf9
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-28 01:09:27 +0300

    s3 v5

--- s3/s3blog/src/components/Blog/BlogPosts.vue
+++ s3/s3blog/src/components/Blog/BlogPosts.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="BlogPosts">
-    <p>testing</p>
+    <span class="posts-views">10</span>
+    <h3>Blog Post Title</h3>
+    <span class="date">date</span>
+    <p class="blog-content">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos libero vitae
+      repellat?
+    </p>
+
+    <div class="row">
+      <div class="col-sm-6">
+        <span class="post-auther">auther name</span>
+      </div>
+      <div class="col-sm-6">
+        <span class="post-catag">category name</span>
+      </div>
+    </div>
   </div>
 </template>
 
