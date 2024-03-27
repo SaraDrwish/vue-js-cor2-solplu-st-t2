0e82cee68f5cf30306688bdfea90e1d85342346d
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-03-28 01:57:24 +0300

    s3 v7

--- s3blog/src/components/Blog/SidePare.vue
+++ s3blog/src/components/Blog/SidePare.vue
@@ -8,4 +8,13 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.SidePare {
+  background: rgb(178, 159, 159);
+  padding: 2rem;
+  margin: 4rem 1rem;
+  border-radius: 1rem;
+  box-shadow: 0 0 14px rgb(52, 73, 80);
+  color: rgb(180, 198, 204);
+}
+</style>
