129601954169f135832b5f66c747aa9a91a001b6
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-02 14:56:40 +0300

    nav using vuetify

---
+++ week5_vuetify_s5/s5-vuetify-v1/src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <!-- <v-toolbar flat app> -->
+      <v-app-bar flat app>
+        <v-toolbar-title class="text-uppercase gray--text">
+          <span class="font-weight-light">Todo</span>
+          <span class=" ">Sara</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text color="gray">
+          <span>SignOut</span>
+          <!-- <v-icon right>exit_to_app</v-icon> -->
+          <!-- <v-icon :icon="`mdiSvg:${mdiAccount}`">exit_to_app</v-icon> -->
+        </v-btn>
+      </v-app-bar>
+
+      <!-- </v-toolbar> -->
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+};
+</script>
+
+<style lang="scss" scoped></style>
