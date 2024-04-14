ab56c440fa51745430a3ab31777a44593bc14cef
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-15 02:42:58 +0300

    aftr ed edi 14

--- week5_vuetify_s5/s5-vuetify-v1/src/views/Projects.vue
+++ week5_vuetify_s5/s5-vuetify-v1/src/views/Projects.vue
@@ -4,14 +4,18 @@
 
     <v-container class="my-5">
       <v-expansion-panels>
-        <v-expansion-panel v-for="(item, i) in 4" :key="i">
+        <v-expansion-panel v-for="pr in projects" :key="pr.title">
+          <!-- <v-expansion-panel v-for="(item, i) in 2" :key="i"> -->
           <v-expansion-panel-header> Project Title </v-expansion-panel-header>
           <v-expansion-panel-content>
-            <v-card flat class="grey--text pa-2">
-              <v-card-title>
-                <div class="font-weight-bold">due XX .XX.XXX</div>
-              </v-card-title>
-              <v-card-text><div>djasldjkasldjakd</div></v-card-text>
+            <div :v-slot="header" class="">{{ pr.title }}</div>
+            <v-card flat>
+              <v-card-text class="grey--text pa-2">
+                <div class="font-weight-bold">
+                  due XX .XX.XXX == {{ pr.due }}
+                </div>
+                <div>{{ pr.content }}</div>
+              </v-card-text>
             </v-card>
           </v-expansion-panel-content>
         </v-expansion-panel>
@@ -23,6 +27,49 @@
 <script>
 export default {
   name: "projects",
+  data() {
+    return {
+      projects: [
+        {
+          title: "sasa",
+          person: "asa Ad",
+          due: "12-12",
+          status: "ongoing",
+          content: "lorem ds ds ds ssssssssssssssssssssssssssssssssssssssss ",
+        },
+        {
+          title: "ammml",
+          person: "neppe",
+          due: "14th-04-2024",
+          status: "completed",
+          content: "lorem15 ",
+        },
+        {
+          title: "lnma",
+          person: "alala",
+          due: "06-06",
+          status: "overdue",
+          content:
+            "dskjksdihkds dfoasjfia jifjsifkjskjfioask ijisfjioksajfkasjf iojdsfkjaskfjskdj ",
+        },
+        {
+          title: "meme",
+          person: "nene",
+          due: "10-10-2024",
+          status: "completed",
+          content:
+            "lorem15lorem15lorem15em15lorem15lorem15lorem15l rem15vlorem15lo5lorem15lorem15",
+        },
+      ],
+    };
+  },
+  computed: {
+    myProjects() {
+      return this.projects.filter((prj) => {
+        return this.prj.person === "nene";
+      });
+    },
+  },
 };
 </script>
 
