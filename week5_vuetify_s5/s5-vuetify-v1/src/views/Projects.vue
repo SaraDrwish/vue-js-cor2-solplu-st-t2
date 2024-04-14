b3a98bc706a969d0a7591fbe6c19e2bd55d1a7de
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-15 02:28:27 +0300

    aftr ed edi 13 v-expansion-panel prijs

--- week5_vuetify_s5/s5-vuetify-v1/src/views/Projects.vue
+++ week5_vuetify_s5/s5-vuetify-v1/src/views/Projects.vue
@@ -3,25 +3,19 @@
     <v-subheader grey--text class="px-4 py-4"> Projects Page</v-subheader>
 
     <v-container class="my-5">
-      <!-- <v-expansion-panels>
-        <v-expansion-panel
-          v-for="i in 4"
-          :key="i"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          title="Item"
-        ></v-expansion-panel>
-      </v-expansion-panels> -->
-      <!-- ///// -->
       <v-expansion-panels>
         <v-expansion-panel v-for="(item, i) in 4" :key="i">
-          <v-expansion-panel-header> Item </v-expansion-panel-header>
+          <v-expansion-panel-header> Project Title </v-expansion-panel-header>
           <v-expansion-panel-content>
-            Lorem ipsum dolor sit exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat.
+            <v-card flat class="grey--text pa-2">
+              <v-card-title>
+                <div class="font-weight-bold">due XX .XX.XXX</div>
+              </v-card-title>
+              <v-card-text><div>djasldjkasldjakd</div></v-card-text>
+            </v-card>
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
-      <!-- ////// -->
     </v-container>
   </div>
 </template>
