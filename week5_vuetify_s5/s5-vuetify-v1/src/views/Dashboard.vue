bc1555ae17da96df790be4cdb3539e1116e0a05f
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-14 15:18:19 +0300

    aftr ed edi

--- week5_vuetify_s5/s5-vuetify-v1/src/views/Dashboard.vue
+++ week5_vuetify_s5/s5-vuetify-v1/src/views/Dashboard.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="dashboard">
-    <h2 class="subheading gray--text">dashboardyyyy page</h2>
-    <v-container class="my-5">
+    <h2 class="subheading gray--text yellow pa-1">dashboardy page</h2>
+    <v-container class="my-5 orange">
       <p>dashboardy container</p>
+      <v-card flat class="pa-2 green">
+        <v-layout row wrap>
+          <v-flex xs12 md6>
+            <div class="caption gray--text red">proj titile</div>
+            <div class="">create a new web</div>
+          </v-flex>
+          <v-flex xs6 ms4 md2>
+            <div class="caption gray--text red">person</div>
+            <div class="">sasas web</div>
+          </v-flex>
+          <v-flex xs6 ms4 md2>
+            <div class="caption gray--text red">due by</div>
+            <div class="">14th-04-2024</div>
+          </v-flex>
+        </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
