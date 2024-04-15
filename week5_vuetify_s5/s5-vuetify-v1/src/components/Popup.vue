c0df1bc956f51768fd8b7651893cde961d57660d
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-15 21:27:34 +0300

    aftr ed edi 23 errr

--- week5_vuetify_s5/s5-vuetify-v1/src/components/Popup.vue
+++ week5_vuetify_s5/s5-vuetify-v1/src/components/Popup.vue
@@ -3,8 +3,8 @@
     <v-dialog max-width="600px">
       <template v-slot:activator="{ on, attrs }">
         <v-btn text class="success" v-bind="attrs" v-on="on"
-          >Add a New Project
-        </v-btn>
+          >Add a New Project</v-btn
+        >
       </template>
       <v-card>
         <v-card-title>
@@ -13,25 +13,21 @@
         <v-card-text>
           <v-form class="pa-3" ref="form">
             <v-text-field
-              :rules="inputRules"
+              :rules="[inputRules]"
+              v-model.trim="title"
               label="Title"
-              v-model="title"
               prepend-icon="mdi-folder"
-            >
-            </v-text-field>
+            ></v-text-field>
             <v-textarea
-              :rules="inputRules"
+              :rules="[inputRules]"
+              v-model.trim="info"
               label="Information"
-              v-model="info"
               prepend-icon="mdi-pencil"
-            >
-            </v-textarea>
-            <!-- /// -->
+            ></v-textarea>
             <v-menu offset-y>
               <template v-slot:activator="{ on, attrs }">
                 <v-text-field
-                  :rules="inputRules"
-                  :v-slot="activator"
+                  :rules="[inputRules]"
                   v-bind="attrs"
                   v-on="on"
                   label="Due Date"
@@ -41,7 +37,6 @@
               </template>
               <v-date-picker v-model="due"></v-date-picker>
             </v-menu>
-            <!-- /// -->
             <v-btn @click="submit" text class="success mt-3 mx-0"
               >Add Project</v-btn
             >
@@ -54,9 +49,7 @@
 
 <script>
 import format from "date-fns/format";
-import db from "../fb.js";
-// import { getFirestore } from "firebase/firestore";
-
+import database from "../fb";
 export default {
   name: "Popup",
   data() {
@@ -64,34 +57,51 @@ export default {
       title: "",
       info: "",
       due: null,
-      inputRules: [(v) => v.length >= 3 || "min length has to be more than 3"],
+      inputRules: [
+        {
+          validator: this.validateLength,
+          message: "min length has to be more than 3",
+        },
+      ],
     };
   },
   methods: {
-    async submit() {
+    validateLength(val) {
+      return val.length >= 3 || "min length has to be more than 3";
+    },
+    submit() {
+      // if (this.$refs.form.validate()) {
+      // console.log("heeeee");
+      // console.log(this.title, this.info, "heeeee");
+      // const PopProject = {
+      //   title: this.title,
+      //   content: this.info,
+      //   due: format(this.due, "Do MMM yyy"),
+      //   person: this.person,
+      //   status: "ongoing",
+      // };
+
+      // const database = database.database();
+      //   const PopProject = database.ref("data");
+      //   PopProject.push({
+      //     title: this.title,
+      //     content: this.info,
+      //     due: format(this.due, "Do MMM yyy"),
+      //     person: this.person,
+      //     status: "ongoing",
+      //   });
+      //   console.log("databass");
+      // }
       if (this.$refs.form.validate()) {
-        console.log(this.title, this.info, "heeeee");
-        const PopProject = {
+        const PopProject = database.ref("data");
+        PopProject.push({
           title: this.title,
-          // Changed variable name from content to info
           content: this.info,
           due: format(this.due, "Do MMM yyy"),
-          person: "this.person",
+          person: "sasaaa",
           status: "ongoing",
-        };
-
-        try {
-          // Used await with db.collection("projects").add(PopProject)
-          const docRef = await db.collection("projects").add(PopProject);
-          // Logged the ID of the added document upon successful addition
-          // Reset form fields after successful submission
-          this.title = "";
-          this.info = "";
-          this.due = null;
-          console.log("Document written with ID: ", "lalalalalla");
-        } catch (error) {
-          console.error("Error adding document: ", error);
-        }
+        });
+        console.log("Project added to database");
       }
     },
   },
--- week5_vuetify_s5/s5-vuetify-v1/src/fb.js
+++ week5_vuetify_s5/s5-vuetify-v1/src/fb.js
@@ -25,12 +25,85 @@
 
 // // //////////
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 
-const config = {
+// const config = {
+//   apiKey: "AIzaSyA-43fKVq96bJqI5_9ICavQwuAcjSNnhkM",
+//   authDomain: "study-vuetify-week5.firebaseapp.com",
+//   projectId: "study-vuetify-week5",
+//   storageBucket: "study-vuetify-week5.appspot.com",
+//   messagingSenderId: "250793869870",
+//   appId: "1:250793869870:web:63887ce44b375412c9d5bf",
+//   measurementId: "G-6WRQTDTWTY",
+// };
+
+// const firebaseApp = initializeApp(config);
+// const db = getFirestore(firebaseApp);
+
+// // Set Firestore settings
+// db.settings({ timestampsInSnapshots: true });
+
+// export default db;
+
+// //////////////////////////////////
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA-43fKVq96bJqI5_9ICavQwuAcjSNnhkM",
+//   authDomain: "study-vuetify-week5.firebaseapp.com",
+//   projectId: "study-vuetify-week5",
+//   storageBucket: "study-vuetify-week5.appspot.com",
+//   messagingSenderId: "250793869870",
+//   appId: "1:250793869870:web:63887ce44b375412c9d5bf",
+//   measurementId: "G-6WRQTDTWTY",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export default analytics;
+
+// //////////////////////////////////
+
+// import firebase from "firebase/app";
+// import "firebase/database";
+
+// // import database from "./fb.js";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA-43fKVq96bJqI5_9ICavQwuAcjSNnhkM",
+//   authDomain: "study-vuetify-week5.firebaseapp.com",
+//   databaseURL: "https://study-vuetify-week5.firebaseio.com",
+//   projectId: "study-vuetify-week5",
+//   storageBucket: "study-vuetify-week5.appspot.com",
+//   messagingSenderId: "250793869870",
+//   appId: "1:250793869870:web:63887ce44b375412c9d5bf",
+//   measurementId: "G-6WRQTDTWTY",
+// };
+
+// const app = firebase.initializeApp(firebaseConfig);
+// const database = app.database();
+
+// export default database;
+
+// //////////////////////
+
+import firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
   apiKey: "AIzaSyA-43fKVq96bJqI5_9ICavQwuAcjSNnhkM",
   authDomain: "study-vuetify-week5.firebaseapp.com",
+  databaseURL: "https://study-vuetify-week5.firebaseio.com",
   projectId: "study-vuetify-week5",
   storageBucket: "study-vuetify-week5.appspot.com",
   messagingSenderId: "250793869870",
@@ -38,10 +111,8 @@ const config = {
   measurementId: "G-6WRQTDTWTY",
 };
 
-const firebaseApp = initializeApp(config);
-const db = getFirestore(firebaseApp);
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const database = app.database();
 
-// Set Firestore settings
-db.settings({ timestampsInSnapshots: true });
-
-export default db;
+export default database;
