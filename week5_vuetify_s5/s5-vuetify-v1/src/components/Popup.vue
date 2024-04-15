4133ee1dffe28b8b64aac876fda55cadce690ce9
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-15 16:09:54 +0300

    aftr ed edi 19

--- week5_vuetify_s5/s5-vuetify-v1/src/components/Popup.vue
+++ week5_vuetify_s5/s5-vuetify-v1/src/components/Popup.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- https://stackoverflow.com/questions/29820791/git-ignore-node-modules-folder-everywhere -->
     <v-dialog max-width="600px">
       <template v-slot:activator="{ on, attrs }">
         <v-btn text class="success" v-bind="attrs" v-on="on"
@@ -24,6 +25,21 @@
               prepend-icon="mdi-pencil"
             >
             </v-textarea>
+            <!-- /// -->
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  :v-slot="activator"
+                  v-bind="attrs"
+                  v-on="on"
+                  label="Due Date"
+                  prepend-icon="mdi-calendar-blank"
+                  :value="formatedDate"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="due"></v-date-picker>
+            </v-menu>
+            <!-- /// -->
             <v-btn @click="submit" text class="success mt-3 mx-0"
               >Add Project</v-btn
             >
@@ -35,17 +51,24 @@
 </template>
 
 <script>
+import format from "date-fns/format";
 export default {
   name: "Popup",
   data() {
     return {
       title: "",
       info: "",
+      due: null,
     };
   },
   methods: {
     submit() {
       console.log(this.title, this.info, "heeeee");
+    },
+  },
+  computed: {
+    formatedDate() {
+      return this.due ? format(this.due, "Do MMM YYY") : "";
     },
   },
 };
